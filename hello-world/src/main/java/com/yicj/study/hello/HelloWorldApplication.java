db9d801486714f65071fe9113020f7db34a1d8df
易成杰 <626659321@qq.com>
2023-10-26 21:26:10 +0800

    init

---
+++ hello-world/src/main/java/com/yicj/study/hello/HelloWorldApplication.java
@@ -0,0 +1,12 @@
+package com.yicj.study.hello;
+
+/**
+ * @author yicj
+ * @date 2023/10/26 21:25
+ */
+public class HelloWorldApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
