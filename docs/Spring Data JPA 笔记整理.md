baff6f57bd77fbdbb877046a8d7d3fbf969636db
易成杰 <626659321@qq.com>
2023-10-28 22:48:19 +0800

    simple demo

--- docs/Spring Data JPA 笔记整理.md
+++ docs/Spring Data JPA 笔记整理.md
@@ -1,3 +1,7 @@
 1. JpaRepositoriesAutoConfiguration
 2. JpaRepositoryFactoryBean
 3. JpaRepositoryFactory
+4. JpaRepositoryConfigExtension
+### 动态代理
+1. JdkDynamicAopProxy
+2. ReflectiveMethodInvocation
--- hello-world/src/main/java/com/yicj/study/hello/HelloWorldApplication.java
+++ hello-world/src/main/java/com/yicj/study/hello/HelloWorldApplication.java
@@ -2,11 +2,13 @@ package com.yicj.study.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * @author yicj
  * @date 2023/10/26 21:25
  */
+@EnableJpaRepositories
 @SpringBootApplication
 public class HelloWorldApplication {
 
