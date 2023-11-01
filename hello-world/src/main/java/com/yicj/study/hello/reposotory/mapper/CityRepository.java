3b0cf8917e0ada0f057bc282ef666802e8e9299b
yichengjie <626659321@qq.com>
2023-11-01 21:04:04 +0800

    init

--- hello-world/src/main/java/com/yicj/study/hello/reposotory/mapper/CityRepository.java
+++ hello-world/src/main/java/com/yicj/study/hello/reposotory/mapper/CityRepository.java
@@ -1,9 +1,8 @@
 package com.yicj.study.hello.reposotory.mapper;
 
 import com.yicj.study.hello.reposotory.entity.City;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -11,7 +10,7 @@ import org.springframework.stereotype.Repository;
  * @date 2023/10/26 21:36
  */
 @Repository
-public interface CityRepository extends JpaRepository<City, Long> {
+public interface CityRepository extends JpaRepository<City, Long>, QuerydslPredicateExecutor<City> {
 
     //Page<City> findAll(Pageable pageable);
 
