c93e2af0c52c9ca6caa97c1db35cf59c85d9d3fb
yichengjie <626659321@qq.com>
2023-11-02 13:15:08 +0800

    save

--- hello-aop/src/test/java/com/yicj/study/aop/service/HelloServiceTest.java
+++ hello-aop/src/test/java/com/yicj/study/aop/service/HelloServiceTest.java
@@ -3,6 +3,7 @@ package com.yicj.study.aop.service;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @Slf4j
@@ -11,6 +12,15 @@ public class HelloServiceTest {
     @Test
     public void context1(){
         ApplicationContext context = new ClassPathXmlApplicationContext("aop.xml") ;
+        HelloService helloService = context.getBean(HelloService.class);
+        String retValue = helloService.hello("张三");
+        log.info("ret value : {}", retValue);
+    }
+
+
+    @Test
+    public void context2(){
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.yicj.study.aop") ;
         HelloService helloService = context.getBean(HelloService.class);
         String retValue = helloService.hello("张三");
         log.info("ret value : {}", retValue);
