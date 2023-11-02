2e5edf063b0af35fee2a722be22bf935c78538e4
yichengjie <626659321@qq.com>
2023-11-02 12:01:20 +0800

    save

--- hello-aop/src/main/java/com/yicj/study/aop/service/impl/HelloServiceImpl.java
+++ hello-aop/src/main/java/com/yicj/study/aop/service/impl/HelloServiceImpl.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
+@Service
 public class HelloServiceImpl implements HelloService {
     @Override
     public String hello(String name) {
